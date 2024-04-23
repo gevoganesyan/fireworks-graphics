e290754294e4f310d3cb65f2bed5796a509bb692
Gev Oganesyan <oganesyangev99@mail.ru>
2024-04-23 20:53:45 +0300

    some updates
    add arg.parse in main.py

--- fireworks/main.py
+++ fireworks/main.py
@@ -1,6 +1,17 @@
+import argparse
 from game import Game
 
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-s", "--sparks-amount-range", action="store_true", help=""
+    )
+    arguments = parser.parse_args()
+    return arguments
 
 if __name__ == '__main__':
     application = Game()
     application.run()
+    main()
